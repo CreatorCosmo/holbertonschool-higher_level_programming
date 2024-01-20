5dbc4a01439f473215a2af3a875824389e14be96
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-01-19 23:16:59 -0400

    File Created

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+w_first3 = word[:3]
+w_last2 = word[-2:]
+w_midle = word[1:-1]
+print(f"First 3 letters: {w_first3}")
+print(f"Last 2 letters: {w_last2}")
+print(f"Middle word: {w_midle}")
