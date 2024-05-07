979d0673e330b9f82fc3635e14eea26a82292129
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-07 15:49:08 -0400

    Created file to complete source code. Implemente print of value with the following specifications: #1 first 3 letters, #2 last 2 letters with the first and #3 last letters.

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first3 = (word[:3])
+last2 = (word[-2:])
+middle = (word[1:-1])
+print(f"First 3 letters: {first3}")
+print(f"Last 2 letters: {last2}")
+print(f"Middle word: {middle}")
