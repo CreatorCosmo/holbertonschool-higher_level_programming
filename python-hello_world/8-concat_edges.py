9540cb03bd0e9e693f71c3b13fd0eb325d2a71d8
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-07 21:42:11 -0400

    File Updated: Fixes to syntax

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented programming"):str.find(" language")] + str[:6])
+print(str[str.find("object-oriented programming"):str.find(" language")] + str[str.find(" with"):str.find(" with") + 5] + " " + str[:6])
