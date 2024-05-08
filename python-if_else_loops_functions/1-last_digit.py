128f2fd7aabfe23dd697b775b71a4cc43f910f20
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-08 10:38:53 -0400

    File updated: Implemented a loop to correctly assign a positive or negative last digit instead of absule value.

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastdigit = abs(number) % 10
+if number < 0:
+    lastdigit = number % -10
+else:
+    lastdigit = number % 10
 
 if lastdigit > 5:
     print(f"Last digit of {number} is {lastdigit} and is greater than 5")
@@ -9,4 +12,3 @@ elif lastdigit < 6 and lastdigit != 0:
     print(f"Last digit of {number} is {lastdigit} and is less than 6 and not 0")
 else:
     print(f"Last digit of {number} is {lastdigit} and is 0")
-    
