520ea233b2935e3abeafd8287537fb4dd13e69e2
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-07 21:58:53 -0400

    File Reversed: Rolled back to previous version

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented programming"):str.find(" language")] +
-      str[str.find(" with"):str.find(" with") + 5] + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(str[str.find("object-oriented programming"):str.find(" language")] + str[str.find(" with"):str.find(" with") + 5] + " " + str[:6])
