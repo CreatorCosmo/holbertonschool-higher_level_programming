02cdf4bead8ba06733da3f39033ef22b952ae49b
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-07 21:30:46 -0400

    File Updated: Renamed variable

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "Python is an interpreted, interactive, object-oriented programming\
+sentence = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str1[39:67] + str1[107:111] + " " + str1[0:6])
+print(sentence[39:67] + sentence[107:111] + " " + sentence[0:6])
