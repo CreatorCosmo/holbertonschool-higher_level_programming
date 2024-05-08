7572f171b5649cacc95fd64d8e76f6e38f025ae6
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-08 15:49:32 -0400

    Created a program to print all possible different combinations of two digits

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for e in range(0, 10):
+    for a in range(1, 10):
+        if e >= a:
+            continue
+        elif e == 8 and a == 9:
+            print("{}{}".format(e, a))
+        else:
+            print("{}{}, ".format(e, a), end="")
