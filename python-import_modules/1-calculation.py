14315dc413637ccb9198f948505a3200d02a9aac
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-10 23:47:12 -0400

    File Updated: Implemented .format syntax

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-
 a = 10
 b = 5
-
-print(add(a, b))  # 15[1]
-print(sub(a, b))  # 5[1]
-print(mul(a, b))  # 50[1]
-print(div(a, b))  # 2.0[1]
+print("Addition result: {}".format(add(a, b)))  # 15[1]
+print("Subtraction result: {}".format(sub(a, b)))  # 5[1]
+print("Multiplication result: {}".format(mul(a, b)))  # 50[1]
+print("Division result: {}".format(div(a, b)))  # 2.0[1]
