e718a080e88bd1e05b92c55ac917482a70916224
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-10 23:49:27 -0400

    File Updated: Implemented conditonal block to execute

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,7 +2,9 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print("Addition result: {}".format(add(a, b)))  # 15[1]
-print("Subtraction result: {}".format(sub(a, b)))  # 5[1]
-print("Multiplication result: {}".format(mul(a, b)))  # 50[1]
-print("Division result: {}".format(div(a, b)))  # 2.0[1]
+
+if __name__ == "__main__":
+    print('Addition result: {}'.format(add(a, b)))
+    print('Subtraction result: {}'.format(sub(a, b)))
+    print('Multiplication result: {}'.format(mul(a, b)))
+    print('Division result: {}'.format(div(a, b)))
