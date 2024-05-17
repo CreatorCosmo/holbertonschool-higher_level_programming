6be2b785ec49517bdecff409b51dfe5fd9b2a2f5
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-17 18:28:48 -0400

    File Updated: Added documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
-class Square:
+"""Defines a class: Square.
 
+This module provides a simple example of an empty class.
+"""
+
+class Square:
+    """An empty class that defines a square.
+
+    This class does not provide any methods or attributes except for those provided by Python by default.
+    """
     pass
