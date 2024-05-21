19b969a9c49c24b819d5afa7f698fb3c59904b82
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-21 11:28:28 -0400

    Created a class that defines a rectangle. Implemented error handling so values can be integers and greater or equal to 0

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" This module contains a class
+    named Rectangle
+"""
+
+class Rectangle:
+    """ Class that is defining Rectangle """
+
+    "Private instance for width"
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        "Property to retrieve the width"
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        "Property to set the width"
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        
+        elif self.__width < 0:
+            raise TypeError('width must be >= 0')
+        else:
+            self.__width = value
+    
+    
+    def height(self):
+        "Property to retrieve height"
+        return self.__height
+    
+    @height.setter
+    def height (self, value):
+        "Property to set the heigth"
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise TypeError('height must be >= 0')
+        else:
+            self.__height = value
