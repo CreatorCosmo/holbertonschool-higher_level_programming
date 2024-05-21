acba1f29c5337466718ba3552c58bcc68da48be5
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-21 10:45:50 -0400

    Made some changes to comments

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" This module contains an empty class named Rectangle"""
+""" This module contains an empty class
+    named Rectangle
+"""
 
 class Rectangle:
     """ Class that is defining Rectangle """
