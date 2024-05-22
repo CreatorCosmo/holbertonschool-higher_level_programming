667a1ddb31d14018ebaae66b7660962351f9918c
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-22 17:26:31 -0400

    Added more documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""This file contain a function that looks
-up the attributes and methods of an object
+"""This file contain a function called
+lookup
 """
 def lookup(obj):
+    """Function that get all attributes and methods of a class
+    Args:
+        obj (class): class parameter
 
+    Returns:
+        list: list with all attributes and methods of the class
+    """
     return dir(obj)
