f1ed742628d7c60cb0c0e22f033014a7f1fa428d
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-28 12:37:24 -0400

    Corrected some errors in the syntax

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -14,8 +14,8 @@ new items to it.
 import sys
 import os.path
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 list_readed = []
 list_1 = sys.argv[1:]
