c9567e1269119f88566f8e0dd3573ad1a2df1f06
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-28 12:16:02 -0400

    Created  a function that creates an Object from a “JSON file”

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contain a function called load_from_json:file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """ function that load a json file
+
+    Args:
+        filename (json): json file
+    """
+    with open(filename, encoding="utf-8") as a_file:
+        return json.loads(a_file.read())
