024333902bec0a856e71f68af01aee7913e2d942
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-28 11:05:16 -0400

    Created a function that appends a string at the end of a text file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module contain a function called append_write"""
+
+
+def append_write(filename="", text=""):
+    """ function that appends a 
+    string at the end of a text file 
+    and returns the number of characters added
+
+    Args:
+        filename (str, optional): [description]. Defaults to "".
+        text (str, optional): [description]. Defaults to "".
+    """
+    with open("{}".format(filename), "a", encoding="utf-8") as a_file:
+        nb_characters_added = a_file.write(text)
+        return nb_characters_added
