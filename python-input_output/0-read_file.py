adb80077b09e79c8c0c172084266d81e638d6e47
DESKTOP-JIPPV9B\giriz <girizarrymartell14@gmail.com>
2024-05-28 10:38:16 -0400

    Created a function that reads a text file (UTF8) and prints it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains a function called read_file"""
+
+
+def read_file(filename=""):
+    """ Function that reads a file
+
+    Args:
+        filename (str, optional): file name string. Defaults to "".
+    """
+    with open(filename, encoding='utf-8') as a_file:
+        line = a_file.read()
+        print(line, end="")
